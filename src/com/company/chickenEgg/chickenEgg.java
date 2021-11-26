93bb12d277df96c91ced9d7aef6bfb76716d1fc3
Artem3423 <art20032345@gmail.com>
2021-11-26 09:01:51 +0300

    add chickenEgg

---
+++ src/com/company/chickenEgg/chickenEgg.java
@@ -0,0 +1,19 @@
+package com.company.chickenEgg;
+
+public class chickenEgg {
+    public static  void main(String[] args){
+        Emergence egg = (new Emergence("яйцо"));
+        Emergence chicken = (new Emergence("курица"));
+        try {
+            egg.thread.join();}
+        catch (InterruptedException e){
+            e.printStackTrace();;
+        }
+        System.out.print("Первым появилось");
+        if (chicken.thread.isAlive()) {
+            System.out.print(" Курица");
+        }else {
+            System.out.println(" Яйцо");
+            }
+        }
+    }
